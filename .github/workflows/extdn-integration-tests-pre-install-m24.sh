3526b062cbfb389244312907cdb234e72e1e94af
Jisse Reitsma <jisse@yireo.com>
2022-05-02 10:19:39 +0200

    Pending changes

--- .github/workflows/extdn-integration-tests-pre-install-m24.sh
+++ .github/workflows/extdn-integration-tests-pre-install-m24.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 composer config minimum-stability dev
 composer config prefer-stable false
+mkdir -p /tmp/composer-files
+
+echo "Composer cache before: `composer -g config cache-files-dir`"
+composer -g config cache-files-dir /tmp/composer-files
+echo "Composer cache after: `composer -g config cache-files-dir`"
 
 composer require yireo/magento2-replace-bundled:^4.0 --no-update
